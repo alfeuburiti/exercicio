81b6285fc725dff623113138be2385a15fae2db2
Fernando Henrique Guimarães <fernandohenriqueguimaraes@gmail.com>
2018-12-26 23:37:06 -0200

    Minor Entities Exceptions

---
+++ src/API-REST/siscap-api/src/main/java/br/gov/pe/reuso/api/service/exception/RegionalComNomeJaExistenteException.java
@@ -0,0 +1,7 @@
+package br.gov.pe.reuso.api.service.exception;
+
+public class RegionalComNomeJaExistenteException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+}
